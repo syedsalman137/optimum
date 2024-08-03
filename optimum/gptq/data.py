169ff02f520a1a8c79e396d5007ad9e184772c40
Syed Salman <quadrisyedsalmanhabeeb@gmail.com>
2024-08-04 04:25:35 +0530

    Update data.py

--- optimum/gptq/data.py
+++ optimum/gptq/data.py
@@ -145,7 +145,7 @@ def get_c4(tokenizer: Any, seqlen: int, nsamples: int, split: str = "train"):
         while True:
             i = random.randint(0, len(data) - 1)
             enc = tokenizer(data[i]["text"], return_tensors="pt")
-            if enc.input_ids.shape[1] >= seqlen:
+            if enc.input_ids.shape[1] > seqlen+1:
                 break
         i = random.randint(0, enc.input_ids.shape[1] - seqlen - 1)
         j = i + seqlen
